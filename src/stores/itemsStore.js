8394d50b539747c4c4f446b4b2964eb49d6f7fe0
Antonia Konsolaki <antonia.konsolaki@arhs-dev-hellas.com>
2024-09-12 12:10:08 +0300

    refactor: Update Zustand implementation and add persist middleware
    Refactor the useItemStore in itemsStore.js to update the Zustand implementation. Add the persist middleware to enable state persistence across page reloads.

--- src/stores/itemsStore.js
+++ src/stores/itemsStore.js
@@ -1,48 +1,51 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { defaultItems } from "../lib/constants";
 
-export const useItemStore = create((set) => ({
-  items: defaultItems,
-  addItem: (newItemText) => {
-    set((state) => {
-      const newItem = {
-        id: new Date().getTime(),
-        text: newItemText,
-        packed: false,
-      };
-      return { items: [...state.items, newItem] };
-    });
-  },
-  removeItem: (id) => {
-    set((state) => ({
-      items: state.items.filter((item) => item.id !== id),
-    }));
-  },
-  toggleItem: (id) => {
-    set((state) => {
-      const newItems = state.items.map((item) => {
-        if (item.id === id) {
-          return { ...item, packed: !item.packed };
-        }
-        return item;
+export const useItemStore = create(
+  persist((set) => ({
+    items: defaultItems,
+    addItem: (newItemText) => {
+      set((state) => {
+        const newItem = {
+          id: new Date().getTime(),
+          text: newItemText,
+          packed: false,
+        };
+        return { items: [...state.items, newItem] };
       });
-      return { items: newItems };
-    });
-  },
-  removeAllItems: () => {
-    set(() => ({ items: [] }));
-  },
-  resetToInitial: () => {
-    set(() => ({ items: defaultItems }));
-  },
-  markAllAsComplete: () => {
-    set((state) => ({
-      items: state.items.map((item) => ({ ...item, packed: true })),
-    }));
-  },
-  markAllAsIncomplete: () => {
-    set((state) => ({
-      items: state.items.map((item) => ({ ...item, packed: false })),
-    }));
-  },
-}));
+    },
+    removeItem: (id) => {
+      set((state) => ({
+        items: state.items.filter((item) => item.id !== id),
+      }));
+    },
+    toggleItem: (id) => {
+      set((state) => {
+        const newItems = state.items.map((item) => {
+          if (item.id === id) {
+            return { ...item, packed: !item.packed };
+          }
+          return item;
+        });
+        return { items: newItems };
+      });
+    },
+    removeAllItems: () => {
+      set(() => ({ items: [] }));
+    },
+    resetToInitial: () => {
+      set(() => ({ items: defaultItems }));
+    },
+    markAllAsComplete: () => {
+      set((state) => ({
+        items: state.items.map((item) => ({ ...item, packed: true })),
+      }));
+    },
+    markAllAsIncomplete: () => {
+      set((state) => ({
+        items: state.items.map((item) => ({ ...item, packed: false })),
+      }));
+    },
+  }))
+);
