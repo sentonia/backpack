aabd37cff61b7e7e9a24733ffd0a039cc8b75987
Antonia Konsolaki <antonia.konsolaki@arhs-dev-hellas.com>
2024-09-12 12:20:06 +0300

    refactor: Refactor Zustand implementation and add persist middleware

--- src/stores/itemsStore.js
+++ src/stores/itemsStore.js
@@ -3,49 +3,63 @@ import { persist } from "zustand/middleware";
 import { defaultItems } from "../lib/constants";
 
 export const useItemStore = create(
-  persist((set) => ({
-    items: defaultItems,
-    addItem: (newItemText) => {
-      set((state) => {
+  persist(
+    (set) => ({
+      items: defaultItems,
+      addItem: (newItemText) => {
         const newItem = {
           id: new Date().getTime(),
           text: newItemText,
           packed: false,
         };
-        return { items: [...state.items, newItem] };
-      });
-    },
-    removeItem: (id) => {
-      set((state) => ({
-        items: state.items.filter((item) => item.id !== id),
-      }));
-    },
-    toggleItem: (id) => {
-      set((state) => {
-        const newItems = state.items.map((item) => {
-          if (item.id === id) {
-            return { ...item, packed: !item.packed };
-          }
-          return item;
+
+        set((state) => ({ items: [...state.items, newItem] }));
+      },
+      removeItem: (id) => {
+        set((state) => {
+          const newItems = state.items.filter((item) => item.id !== id);
+          return { items: newItems };
         });
-        return { items: newItems };
-      });
-    },
-    removeAllItems: () => {
-      set(() => ({ items: [] }));
-    },
-    resetToInitial: () => {
-      set(() => ({ items: defaultItems }));
-    },
-    markAllAsComplete: () => {
-      set((state) => ({
-        items: state.items.map((item) => ({ ...item, packed: true })),
-      }));
-    },
-    markAllAsIncomplete: () => {
-      set((state) => ({
-        items: state.items.map((item) => ({ ...item, packed: false })),
-      }));
-    },
-  }))
+      },
+      toggleItem: (id) => {
+        set((state) => {
+          const newItems = state.items.map((item) => {
+            if (item.id === id) {
+              return { ...item, packed: !item.packed };
+            }
+
+            return item;
+          });
+          return { items: newItems };
+        });
+      },
+      removeAllItems: () => {
+        set(() => ({ items: [] }));
+      },
+      resetToInitial: () => {
+        set(() => ({ items: defaultItems }));
+      },
+      markAllAsComplete: () => {
+        set((state) => {
+          const newItems = state.items.map((item) => {
+            return { ...item, packed: true };
+          });
+
+          return { items: newItems };
+        });
+      },
+      markAllAsIncomplete: () => {
+        set((state) => {
+          const newItems = state.items.map((item) => {
+            return { ...item, packed: false };
+          });
+
+          return { items: newItems };
+        });
+      },
+    }),
+    {
+      name: "items",
+    }
+  )
 );
